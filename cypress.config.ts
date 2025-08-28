b8151280e9f2df7546926a7f9f3bed03fde2a30f
gitwilma <wilma.haakansson@gmail.com>
2025-08-28 11:40:43 +0200

    rename duplicate var name

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,15 +2,18 @@ import { spawn } from "child_process";
 import { defineConfig } from "cypress";
 import { MongoMemoryReplSet } from "mongodb-memory-server";
 import waitOn from "wait-on";
+import { db } from "./prisma/db";
 import { seedTodos } from "./prisma/seed/todo";
 
 export default defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       // implement node event listeners here
+
       // 1. Skapa en in-memory databas (replica set, prisma gnäller annars)
-      const db = await MongoMemoryReplSet.create({ replSet: { count: 1 } });
-      const dbUri = db.getUri("cypress-test");
+      const mongo = await MongoMemoryReplSet.create({ replSet: { count: 1 } });
+      const dbUri = mongo.getUri("cypress-test");
+
       // 2. Starta Next.js servern (på en annan port som ansluter till 1.)
       const server = spawn("npx", ["next", "dev", "--turbopack", "-p", "3100"], {
         env: {
@@ -19,19 +22,18 @@ export default defineConfig({
         },
         stdio: "inherit",
       });
-
+      
       // 3. Vänta på att Next.js servern är igång innan cypress kör vidare
         await waitOn({ "resources": ["http://localhost:3100"], timeout: 60_000 });
 
       // 4. Städa upp processerna dvs Mongo databasen och Next.s-servern
       const cleanup = async () => {
         server.kill();
-        await db.stop();
+        await mongo.stop();
       };
       process.on("exit", cleanup);
 
       // 5. Reseeda om databasen så att testerna blir obereonde av varandra.
-
       on("task", {
         async reseed() {
           await db.todo.deleteMany();
