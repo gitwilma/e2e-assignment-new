e644bd2a856aceca40e39d2f213a0e1e8670aff5
gitwilma <wilma.haakansson@gmail.com>
2025-08-28 14:40:12 +0200

    functioning tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,45 +2,47 @@ import { spawn } from "child_process";
 import { defineConfig } from "cypress";
 import { MongoMemoryReplSet } from "mongodb-memory-server";
 import waitOn from "wait-on";
-import { seedTodos } from "./prisma/seed/todo";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost3100",
+    baseUrl: "http://localhost:3100",
     async setupNodeEvents(on, config) {
       // implement node event listeners here
-
+      
       // 1. Skapa en in-memory databas (replica set, prisma gnäller annars)
       const mongo = await MongoMemoryReplSet.create({ replSet: { count: 1 } });
       const dbUri = mongo.getUri("cypress-test");
-
+      
       // 2. Starta Next.js servern (på en annan port som ansluter till 1.)
       const server = spawn("npx", ["next", "dev", "--turbopack", "-p", "3100"], {
         env: {
+          ...process.env,
           NODE_ENV: "test", 
           DATABASE_URL: dbUri
         },
         stdio: "inherit",
       });
-
+      
       // 3. Vänta på att Next.js servern är igång innan cypress kör vidare
-        await waitOn({ "resources": ["http://localhost:3100"], timeout: 60_000 });
-
+      await waitOn({ "resources": ["http://localhost:3100"], timeout: 60_000 });
+      
       // 4. Städa upp processerna dvs Mongo databasen och Next.s-servern
       const cleanup = async () => {
         server.kill();
         await mongo.stop();
       };
-      process.on("exit", cleanup);
-
+      on("after:run", cleanup);
+      process.on("SIGTERM", cleanup);
+      
       // 5. Reseeda om databasen så att testerna blir obereonde av varandra.
       process.env.DATABASE_URL = dbUri;
       on("task", {
         async reseed() {
           const { db } = await import("./prisma/db");
+          const { seedTodos } = await import("./prisma/seed/todo");
           await db.todo.deleteMany();
           await seedTodos();
-
+          
           return null;
         },
       });
