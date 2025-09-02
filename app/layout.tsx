742bde0afb64c01d7c0836d78ceb0416d5603bf3
gitwilma <wilma.haakansson@gmail.com>
2025-09-02 11:32:10 +0200

    added gradient bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="min-h-screen grid place-items-center p-4">
+        <div className="min-h-screen grid place-items-center p-4 bg-gradient-to-b from-[#65060D] to-[#9DA0DD]">
           <div className="w-full max-w-lg rounded-2xl border shadow-sm bg-white/90 dark:bg-neutral-900/60 backdrop-blur p-6">
             {children}
           </div>
